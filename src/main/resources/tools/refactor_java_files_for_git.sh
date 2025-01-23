1d0d1b28d11ad98996c1d0e1e5d03913d4c56ff5
bgold04 <37762452+bgold04@users.noreply.github.com>
2025-01-23 10:13:55 -0500

    moved files around, new pom.xml

---
+++ src/main/resources/tools/refactor_java_files_for_git.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# refactor java files for https://github.com/bgold04/MavenAutoPrimer on 20250123
+
+cd /Users/bgold/MavenAutoPrimer
+
+
+git add src/main/java/com/github/EnsemblGeneCoordinatesFetcher.java
+git add src/main/java/com/github/FastaSequenceRetriever.java
+git add src/main/java/com/github/GeneCoordinatesFetcher.java
+git add src/main/java/com/github/GeneDetailsModel.java
+git add src/main/java/com/github/GeneSearchResultModel.java
+git add src/main/java/com/github/GenomicRegionParser.java
+git add src/main/java/com/github/GenomicRegionSummaryModel.java
+git add src/main/java/com/github/MisprimingLibraryBuilder.java
+git add src/main/java/com/github/Primer3ResultsController.java
+git add src/main/java/com/github/UcscBuildAndTableFetcher.java
+git add src/main/java/com/github/UcscGeneCoordinatesFetcher.java
+git add src/main/resources/tools/
+
